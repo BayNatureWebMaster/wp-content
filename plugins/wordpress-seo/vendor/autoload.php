35ade485bea0c88da917d09c13a58c430b48067e
BayNature <webmaster@baynature.org>
2025-03-06 11:42:29 -0800

    Yoast SEO plugin update
    Yoast SEO plugin update Version 22.8 => 24.6

--- plugins/wordpress-seo/vendor/autoload.php
+++ plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitdb62dfb15ae92d6fb1dd9e6991f40e08::getLoader();
+return ComposerAutoloaderInit5f4a5446ea71a232d9b78e343286134d::getLoader();
