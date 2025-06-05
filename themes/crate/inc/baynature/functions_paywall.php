7167db388c45f0713d39c34240e3f712b6651d1a
BayNature <webmaster@baynature.org>
2025-06-05 11:32:49 -0700

    debug find first pp in pw content
    debug find first pp in pw content

--- themes/crate/inc/baynature/functions_paywall.php
+++ themes/crate/inc/baynature/functions_paywall.php
@@ -16,7 +16,9 @@ function display_the_next_paragraph( $content_str , $searchFor ) {
 		$sub_str = substr(  $content_str , $p1 );
 		// find the end of the paragraph
 		$p2 = strpos( $sub_str , "</p>") + 4;
+		echo "p2 - p1 = ".$p2 - $p1;
 		if ( ($p2 - $p1 ) > 200 ) $not_found = false;
+		$not_found = false;
 	}
 	$the_paragraph = substr( $sub_str , 0, $p2);
 	echo $the_paragraph;
