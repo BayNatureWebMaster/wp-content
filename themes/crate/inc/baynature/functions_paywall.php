3e44881413aaebd0fe76e9b1a3b8b1d5ab9c80da
BayNature <webmaster@baynature.org>
2025-10-23 11:16:15 -0700

    paywall teaser
    debugging

--- themes/crate/inc/baynature/functions_paywall.php
+++ themes/crate/inc/baynature/functions_paywall.php
@@ -117,7 +117,7 @@ function show_member_login_message( $contentType ) {
 	$content_str = get_the_content();
 	//display_article_teaser( $content_str , "<span class=\"end-teaser\">");
 
-	display_article_teaser( $content_str , '<span class="end-teaser">');
+	display_article_teaser( $content_str , 'end-teaser');
 	display_become_a_member_message( $contentType );
 	display_member_login_message();
 }
